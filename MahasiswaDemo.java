b7f1214321d96058707a0dbd7aa72427d59ee913
aretaneilaputri <retaaneila@gmail.com>
2026-09-13 22:56:12 +0700

    3.1 Percobaan 1 Pertanyaan nomor 12

--- MahasiswaDemo.java
+++ MahasiswaDemo.java
@@ -8,6 +8,28 @@ public class MahasiswaDemo {
         
         m1.displayBiodata();
 
+        System.out.println("--------------------------");
+
+        // Objek 2 (soal nomor 12)
+        Mahasiswa m2 = new Mahasiswa();
+        m2.nim = "254107060164";
+        m2.nama = "Areta Neila Putri";
+        m2.alamat = "Malang, Jawa Timur";
+        m2.kelas = "2C";
+
+        m2.displayBiodata();
+
+        System.out.println("--------------------------");
+
+        // Objek 3 
+        Mahasiswa m3 = new Mahasiswa();
+        m3.nim = "25410706016";
+        m3.nama = "Alina Nadhira Putri";
+        m3.alamat = "Bandung, Jawa Barat";
+        m3.kelas = "2B";
+        
+        m3.displayBiodata();
+
         
     }
 }
